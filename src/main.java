b77713302cee72ed6b2c3930c9ad3e6808b07ff3
HalconCosmico <andespej@ucm.es>
2022-03-28 20:29:29 +0200

    si

--- src/main.java
+++ src/main.java
@@ -2,6 +2,9 @@ public class main {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello worqfdafsggld");
     }
 
 }
