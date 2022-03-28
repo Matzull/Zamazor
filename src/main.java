6594245a8eed9cd997e301e571964835143f9314
Matzull <marcal16@ucm.es>
2022-03-28 20:13:03 +0200

    First steps

--- src/main.java
+++ src/main.java
@@ -1,8 +1,7 @@
 public class main {
     public static void main(String[] args)
     {
-        System.out.println("wachugetmiinthesamar");
-        System.out.println("hello world");
+        System.out.println("Hello world");
     }
 
 }
