6cbd50abe0c983e090296738902730a756e75f23
Alexzame <alejazam@ucm.es>
2022-03-29 09:54:53 +0200

    GUI

--- src/View/MainFrameArticles.java
+++ src/View/MainFrameArticles.java
@@ -1,11 +1,14 @@
 package View;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrameArticles extends JFrame{
+    private JPanel mainArticlesPanel;
     private JTextField nArticulo;
     private JButton searchButton;
-    private JPanel mainArticlesPanel;
+    private JLabel zamazor;
 
 
     public MainFrameArticles(){
@@ -14,5 +17,15 @@ public class MainFrameArticles extends JFrame{
         setSize(600,400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Accion boton de busqueda
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
